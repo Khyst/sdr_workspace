eeb4e3c55af99ec380328b467fbca25418306bf0
김용훈 <yonghun@rastech.co.kr>
2026-02-22 08:23:22 +0000

    devcontainer 설정 업데이트 및 초기 설정 스크립트 수정

--- .devcontainer/initial_setup.sh
+++ .devcontainer/initial_setup.sh
@@ -13,6 +13,12 @@ sudo rosdep install --from-paths src --ignore-src -y
 echo "--- Setting permissions ---"
 sudo chown -R $(whoami) /home/${USERNAME}/workspace
 
+echo "--- Building ROS 2 workspace ---"
+source /opt/ros/humble/setup.bash
+cd /home/${USERNAME}/workspace
+colcon build --symlink-install
+echo "source /home/${USERNAME}/workspace/install/local_setup.bash" >> ~/.bashrc
+
 echo "--- add alias to bashrc ---"
 echo "alias sb='source ~/.bashrc'" >> ~/.bashrc
 echo "alias cb='colcon build --symlink-install'" >> ~/.bashrc
